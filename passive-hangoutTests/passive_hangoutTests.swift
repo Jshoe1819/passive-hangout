fab551b06a52992f2010d22aa6af247da61512c0
Jacob Shoemaker <Jshoe1819@gmail.com>
2017-08-08 19:13:58 -0400

    Initial Commit

---
+++ passive-hangoutTests/passive_hangoutTests.swift
@@ -0,0 +1,36 @@
+//
+//  passive_hangoutTests.swift
+//  passive-hangoutTests
+//
+//  Created by Jacob Shoemaker on 8/8/17.
+//  Copyright © 2017 Jacob Shoemaker. All rights reserved.
+//
+
+import XCTest
+@testable import passive_hangout
+
+class passive_hangoutTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
